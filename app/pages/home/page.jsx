71070b1b82c1c102bdff59cdb275ceff143c25f4
Amenyuwel <Amenyuwel@gmail.com>
2025-04-01 22:10:47 +0800

    Small fix

--- app/pages/home/page.jsx
+++ app/pages/home/page.jsx
@@ -179,8 +179,8 @@ const HomePage = () => {
               UPLOAD IMAGE
             </button>
             <span className="text-main mt-8 text-sm">
-              File must be <strong>JPG</strong> or <strong>PNG</strong>, and up
-              to <strong> 100 images.</strong>
+              File must be <strong>JPG</strong> or <strong>PNG</strong>, and a maximum of
+              <strong> 100 images.</strong>
             </span>
           </div>
         </div>
