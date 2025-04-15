e4453f6fa896bd025fd4ac73fe568b09e14a3988
Amenyuwel <Amenyuwel@gmail.com>
2025-04-15 22:38:52 +0800

    Changed the description in descAbout to highightlight AI-REFINE

--- app/pages/about/descAbout.js
+++ app/pages/about/descAbout.js
@@ -1,6 +1,6 @@
 const description = (
     <>
-      Enhance your dataset effortlessly with our automated image augmentation tool. Instantly apply transformations such as <em className="font-semibold italic">grayscale</em>, <em className="font-semibold italic">flipping</em>, <em className="font-semibold italic">brightness adjustment</em>, and <em className="font-semibold italic">more</em> to improve model performance without manual effort. <br/> Perfect for AI training, object detection, and classification tasks.
+      Enhance your dataset effortlessly with AI-REFINE'S automated image augmentation tool. Instantly apply transformations such as <em className="font-semibold italic">grayscale</em>, <em className="font-semibold italic">flipping</em>, <em className="font-semibold italic">brightness adjustment</em>, and <em className="font-semibold italic">more</em> to improve model performance without manual effort. <br/> Perfect for AI training, object detection, and classification tasks.
     </>
   );
   
